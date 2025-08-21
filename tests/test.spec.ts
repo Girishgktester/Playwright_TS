69569fbb2c7f0771e6ed7aaee9931a71ab14d684
Girishgktester <girishgk.tester@gmail.com>
2025-08-21 22:39:24 +0530

    Good day 1

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -6,12 +6,31 @@ test.describe('launch browser', () => {
         await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
 
-        await page.fill('input[name="username"]', "Admin");
-        await page.fill('input[name="password"]', "admin123");
+        await page.locator('input[name="username"]').fill("Admin");
+        await page.locator('input[name="password"]').fill("admin123");
+        await page.waitForTimeout(10);
+        await page.waitForSelector('button[type="submit"]');
+        await page.locator('button[type="submit"]').click();
 
-
-        await page.click('button[type="submit"]');
         await expect(page).toHaveTitle("OrangeHRM");
+    });
+
+
+    test('test', async ({ page }) => {
+        await page.goto('https://letcode.in/');
+        await page.getByRole('link', { name: 'letcode' }).click();
+        await page.getByRole('link', { name: 'Work-Space' }).click();
+        await page.getByRole('link', { name: 'Edit' }).click();
+        await page.getByRole('textbox', { name: 'Enter first & last name' }).click();
+        await page.getByRole('textbox', { name: 'Enter first & last name' }).fill('Test');
+        await page.getByRole('textbox', { name: 'Enter first & last name' }).press('Tab');
+        await page.locator('#join').fill('yes');
+        await page.locator('#join').press('Tab');
+        await page.locator('#getMe').fill('ok');
+        await page.locator('#getMe').press('Tab');
+        await page.locator('#clearMe').fill('Girish');
+        await page.locator('#clearMe').press('Tab');
+        await page.locator('#dontwrite').press('Tab');
     });
 
 });
